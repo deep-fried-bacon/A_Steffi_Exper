a3b8db8f3cf2485bc896e1e05c5262361a4826c4
Amelia <>
2018-01-23 12:31:47 -0500

    upadate

--- A_Steffi_Exper.bat
+++ A_Steffi_Exper.bat
@@ -2,4 +2,4 @@ javac -cp "C:\Users\localuser\Desktop\Baylies Lab\fiji-win64\Fiji.app\jars\*" .\
 javac -cp "C:\Users\localuser\Desktop\Baylies Lab\fiji-win64\Fiji.app\jars\*;.\jsteffi" .\jsteffi\*.java
 javac -cp "C:\Users\localuser\Desktop\Baylies Lab\fiji-win64\Fiji.app\jars\*;." A_Steffi_Exper.java
 jar cf A_Steffi_Exper.jar .
-move /y "C:\Users\localuser\Desktop\Code Laboratory\jsteffi\A_Steffi_Exper\A_Steffi_Exper.jar" "C:\Users\localuser\Desktop\Baylies Lab\fiji-win64\Fiji.app\plugins\A_Steffi_Exper.jar"
+move /y "C:\Amelia\A_Steffi_Exper\A_Steffi_Exper.jar" "C:\Users\localuser\Desktop\Baylies Lab\fiji-win64\Fiji.app\plugins\A_Steffi_Exper.jar"
